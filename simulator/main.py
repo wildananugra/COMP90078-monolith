3f2025d4d7a779224f7cec3ba76ea09ca553d868
wildananugra <wildananugra@gmail.com>
2021-08-02 21:29:04 +0700

    initial

---
+++ simulator/main.py
@@ -0,0 +1,20 @@
+from typing import Optional
+from fastapi import FastAPI, Header, Request
+import json
+
+app = FastAPI()
+
+@app.post("/sim/{merchant_path}")
+async def root(request: Request, action: Optional[str] = Header(None)):
+    print(f"action: {request.headers['Action']}")
+
+    action = request.headers['Action']
+    merchant_path = request.path_params['merchant_path']
+
+    if(merchant_path == "merchant-1"):
+        output = ""
+        with open(f"{merchant_path}/{action}.json",'r') as f:
+            output = f.read()
+        return json.loads(output)
+    else:
+        return { "message" : "error!" }
